f7541987cef35db541465fbb3c024fc9c83b26f7
icet00 <florent.enaux@gmail.com>
2018-03-02 20:53:36 +0100

    basic scraping for one page

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+var json    = [];
+var i       = 1;
+
+const configuration = {
+  'uri': 'https://m.lafourchette.com/fr_FR/search?ll=48.856614,2.3522219000000177&searchText=Paris,%20France&googlePlaceId=ChIJD7fiBh9u5kcRYJSMaMOCCwQ',
+};
+
+app.get('/', function(req, res){
+  res.send("To show");
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
